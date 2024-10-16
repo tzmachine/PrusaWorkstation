dd6a983aaa92a699e4621bceb8be78b17bf13115
tzmachine <thorben.zimmermann.tz@outlook.de>
2024-10-16 21:30:54 +0200

    added support for mutliple buttons

--- webserver/server.py
+++ webserver/server.py
@@ -1,24 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import RPi.GPIO as GPIO
 
 server = Flask(__name__)
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(13, GPIO.OUT)
+# Konfiguriere die GPIO-Pins
+GPIO.setmode(GPIO.BCM)  # Nutze die BCM-Pin-Nummerierung
+gpio_pins = [17, 27, 22, 23]  # Beispiel-Pins für GPIO 1, 2, 3, 4
+for pin in gpio_pins:
+    GPIO.setup(pin, GPIO.OUT)  # Setze alle Pins als Ausgang
 
 @server.route('/')
 def index():
  return render_template('index.html')
 
-@server.route('/print')
-def print():
- return 'Rasperry Pie!'
+@server.route('/toggle_gpio/<int:pin_number>', methods=['POST'])
+def toggle_gpio(pin_number):
+# Überprüfe, ob die pin_number in der Liste gpio_pins ist
+    if pin_number not in gpio_pins:
+        return jsonify({'error': 'Ungültige Pin-Nummer'}), 400
 
-@server.route('/toggle_pin')
-def toggle_pin():
- state = GPIO.input(13)
- GPIO.output(13, not state)
- return 'GPIO Pin 27 set to HIGH'
+    # Wenn die Pin-Nummer gültig ist, finde den Index und toggle den Pin
+    pin_index = gpio_pins.index(pin_number)  # Finde den Index der Pin-Nummer
+    GPIO.output(pin_number, not GPIO.input(pin_number))  # Toggle den Status des Pins
+
+    return jsonify({'status': 'success', 'pin': pin_number})
+
 if __name__ == '__main__':
 
  try:
