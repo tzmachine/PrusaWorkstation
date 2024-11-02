8e13413016c9fd6dfe36ee9a6f960e6b945858ce
tzmachine <thorben.zimmermann.tz@outlook.de>
2024-11-02 15:34:13 +0100

    first steps, action doesn't trigger through browser, but possible in terminal, false user rights to execute, www-data

---
+++ assets/python/gpio_control.py
@@ -0,0 +1,16 @@
+import sys
+import RPi.GPIO as GPIO
+
+# GPIO-Pin-Nummer aus den Argumenten lesen
+#pin = int(sys.argv[1])
+pin = 6
+# GPIO-Modus setzen
+GPIO.setmode(GPIO.BCM)  # BCM-Nummerierung verwenden
+
+# Den Pin konfigurieren
+GPIO.setup(pin, GPIO.OUT)
+
+GPIO.output(pin, not GPIO.input(pin))
+
+# GPIO-Bibliothek zurücksetzen
+#GPIO.cleanup()
